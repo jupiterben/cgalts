2b4c21c8cd6b63db90df6e8cb687953c5ce3cdce
fub <jupiter.ben@live.com>
2021-02-13 02:57:35 +0800

    add

--- src/convexhull/convexhull2.ts
+++ src/convexhull/convexhull2.ts
@@ -1,5 +1,5 @@
 
-import std, { IForwardIterator, Singleton } from "tstl";
+import std, { IForwardIterator } from "tstl";
 import { Sign } from '../enum';
 
 interface IPoint2 {
@@ -37,22 +37,21 @@ interface Convex_hull_traits {
     less_yx_2_object(): Less_yx_2;
 }
 
-
 type ForwardIterator = IForwardIterator<IPoint2>;
 
 interface IOutputIterator<T> {
     value: T;
 };
 
-function ch_graham_andrew(first: ForwardIterator, last: ForwardIterator, ch_traits: Convex_hull_traits) {
-    const V = Array.from(input);
-    V.sort(ch_traits.less_xy_2_object());
+// function ch_graham_andrew(first: ForwardIterator, last: ForwardIterator, ch_traits: Convex_hull_traits) {
+//     const V = Array.from(input);
+//     V.sort(ch_traits.less_xy_2_object());
 
-    const equal_points = ch_traits.equal_2_object();
-    if (equal_points(V[0], V[V.length - 1])) {
-        return [V[0]];
-    }
-}
+//     const equal_points = ch_traits.equal_2_object();
+//     if (equal_points(V[0], V[V.length - 1])) {
+//         return [V[0]];
+//     }
+// }
 
 interface NSWE<T> {
     n?: T;
@@ -83,9 +82,8 @@ function ch_nswe_point<T extends ForwardIterator>(first: T, last: T,
     nswe.e = e;
 }
 
-function ch_nswe_point_with_order<T extends ForwardIterator>(first: T, last: T,
-    nswe: NSWE<T>,
-    ch_traits: Convex_hull_traits) {
+function ch_nswe_point_with_order(first: ForwardIterator, last: ForwardIterator,
+    nswe: NSWE<ForwardIterator>, ch_traits: Convex_hull_traits): ForwardIterator[] {
     ch_nswe_point(first, last, nswe, ch_traits);
     const { n, s, w, e } = nswe;
     const V = new std.Vector([n, s, w, e]);
@@ -118,19 +116,19 @@ function ch_akl_toussaint(first: ForwardIterator, last: ForwardIterator,
     region3.push_back(e.value);
     region4.push_back(n.value);
 
-    const duplicated_exteme_points = (ranges[0] === ranges[1] ? 1 : 0) +
+    const duplicated_extreme_points = (ranges[0] === ranges[1] ? 1 : 0) +
         (ranges[1] === ranges[2] ? 1 : 0) +
         (ranges[2] === ranges[3] ? 1 : 0);
 
-    if (duplicated_exteme_points > 0) {
-        ch_akl_toussaint_assign_points_to_regions_deg(first, ranges[0], left_turn, nswe, region1, region2, region3, region4, duplicated_exteme_points, ch_traits);
+    if (duplicated_extreme_points > 0) {
+        ch_akl_toussaint_assign_points_to_regions_deg(first, ranges[0], left_turn, nswe, region1, region2, region3, region4, duplicated_extreme_points, ch_traits);
         if (ranges[0] !== ranges[1])
-            ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[0]), ranges[1], left_turn, nswe, region1, region2, region3, region4, duplicated_exteme_points, ch_traits);
+            ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[0]), ranges[1], left_turn, nswe, region1, region2, region3, region4, duplicated_extreme_points, ch_traits);
         if (ranges[1] !== ranges[2])
-            ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[1]), ranges[2], left_turn, nswe, region1, region2, region3, region4, duplicated_exteme_points, ch_traits);
+            ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[1]), ranges[2], left_turn, nswe, region1, region2, region3, region4, duplicated_extreme_points, ch_traits);
         if (ranges[2] !== ranges[3])
-            ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[2]), ranges[3], left_turn, nswe, region1, region2, region3, region4, duplicated_exteme_points, ch_traits);
-        ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[3]), last, left_turn, nswe, region1, region2, region3, region4, duplicated_exteme_points, ch_traits);
+            ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[2]), ranges[3], left_turn, nswe, region1, region2, region3, region4, duplicated_extreme_points, ch_traits);
+        ch_akl_toussaint_assign_points_to_regions_deg(std.next(ranges[3]), last, left_turn, nswe, region1, region2, region3, region4, duplicated_extreme_points, ch_traits);
     }
     else {
         ch_akl_toussaint_assign_points_to_regions(first, ranges[0], left_turn, nswe, region1, region2, region3, region4, ch_traits);
@@ -140,6 +138,33 @@ function ch_akl_toussaint(first: ForwardIterator, last: ForwardIterator,
         ch_akl_toussaint_assign_points_to_regions(std.next(ranges[3]), last, left_turn, nswe, region1, region2, region3, region4, ch_traits);
     }
 
+    const less_xy_2_object = ch_traits.less_xy_2_object();
+    const greater = (a, b) => { return less_xy_2_object(b, a); }
+    std.sort(std.next(region1.begin()), region1.end(), less_xy_2_object);
+    std.sort(std.next(region2.begin()), region2.end(), less_xy_2_object);
+    std.sort(std.next(region3.begin()), region3.end(), greater);
+    std.sort(std.next(region4.begin()), region4.end(), greater);
+
+    if (!equal_points(w.value, s.value)) {
+        region1.push_back(s.value);
+        ch__ref_graham_andrew_scan(region1.begin(), region1.end(),
+            result, ch_traits);
+    }
+    if (!equal_points(s.value, e.value)) {
+        region2.push_back(e.value);
+        ch__ref_graham_andrew_scan(region2.begin(), region2.end(),
+            result, ch_traits);
+    }
+    if (!equal_points(e.value, n.value)) {
+        region3.push_back(n.value);
+        ch__ref_graham_andrew_scan(region3.begin(), region3.end(),
+            result, ch_traits);
+    }
+    if (!equal_points(n.value, w.value)) {
+        region4.push_back(w.value);
+        ch__ref_graham_andrew_scan(region4.begin(), region4.end(),
+            result, ch_traits);
+    }
 }
 
 function ch_akl_toussaint_assign_points_to_regions(first: ForwardIterator, last: ForwardIterator,
@@ -218,3 +243,58 @@ function ch_akl_toussaint_assign_points_to_regions_deg(first: ForwardIterator, l
     }
 }
 
+function ch__ref_graham_andrew_scan<BidirectionalIterator, OutputIterator>(first: BidirectionalIterator,
+    last: BidirectionalIterator,
+    result: OutputIterator,
+    ch_traits: Convex_hull_traits) {
+
+    const left_turn = ch_traits.left_turn_2_object();
+
+    const S = new std.Vector<BidirectionalIterator>();
+    BidirectionalIterator              alpha;
+    BidirectionalIterator              beta;
+    BidirectionalIterator              iter;
+    CGAL_ch_precondition(first != last);
+    CGAL_ch_precondition(std:: next(first) != last);
+
+    --last;
+    CGAL_ch_precondition(!equal_points(* first,* last));
+    S.push_back(last);
+    S.push_back(first);
+
+    iter = first;
+    do {
+        ++iter;
+    }
+    while ((iter != last) && !left_turn(* last, * first, * iter));
+
+    if (iter != last) {
+        S.push_back(iter);
+        typedef typename std:: vector<BidirectionalIterator>:: reverse_iterator
+    rev_iterator;
+        rev_iterator  stack_rev_iter = S.rbegin();
+        alpha = iter;
+        beta = * ++stack_rev_iter;
+
+        for (++iter; iter != last; ++iter) {
+            if (left_turn(* alpha, * iter, * last)) {
+                while (!left_turn(* beta, * alpha, * iter)) {
+                    S.pop_back();
+                    alpha = beta;
+                    stack_rev_iter = S.rbegin();
+                    beta = * ++stack_rev_iter;
+                    CGAL_ch_assertion(S.size() >= 2);
+                }
+                S.push_back(iter);
+                beta = alpha;
+                alpha = iter;
+            }
+        }
+
+    }
+
+    typedef typename std:: vector<BidirectionalIterator>:: iterator std_iterator;
+    std_iterator  stack_iter = S.begin();
+    for (++stack_iter; stack_iter != S.end(); ++stack_iter) { * result =  ** stack_iter; ++result; }
+    return result;
+}
